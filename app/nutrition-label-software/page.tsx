8c4f859080be4465b6c20eb3d82c7f2b05b216c7
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2024-01-16 01:02:55 +0900

    fit: add tabs

--- app/nutrition-label-software/page.tsx
+++ app/nutrition-label-software/page.tsx
@@ -2,12 +2,26 @@
 
 import {useState} from "react";
 import TopHero from "@/app/ui/nutrition-label-software/top-hero";
+import AllFeatures from "@/app/ui/nutrition-label-software/tab_content/all_features";
+import Tabs from "@/app/ui/nutrition-label-software/tabs";
 
 export default function NutritionLabelSoftware() {
     const [tabIndex, setTabIndex] = useState(0)
-    const tabs = ["All Features", "Nutrition Analysis", "Labeling", "Costing", "Ingredient Lists", "Recipe Management"];
+    const tabs = {
+        "All Features": <AllFeatures/>,
+        "Nutrition Analysis": <div></div>,
+        "Labeling": <div></div>,
+        "Costing": <div></div>,
+        "Ingredient Lists": <div></div>,
+        "Recipe Management": <div></div>,
+    };
 
     return <div className={"flex flex-col"}>
         <TopHero/>
+        <Tabs
+            activeTabIndex={tabIndex}
+            tabs={Object.keys(tabs)}
+            onTabClick={(i) => setTabIndex(i)}
+        />
     </div>
 }
---
+++ app/ui/nutrition-label-software/tabs.tsx
@@ -0,0 +1,26 @@
+export default function Tabs({activeTabIndex, tabs, onTabClick}: { activeTabIndex: number, tabs: string[], onTabClick: (index: number) => void }) {
+    return <div className={"flex items-center justify-center h-[45px] border-b-[1px] border-b-[#dbdbdb] mt-[1rem] mx-12 mb-16"}>
+        {tabs.map((tab, ti) => {
+            const isActive = ti == activeTabIndex;
+            const extraStyles: { [key: string]: string } = {}
+
+            if (isActive) {
+                extraStyles["borderBottomWidth"] = "1px";
+                extraStyles["color"] = "#408abf";
+            }
+
+            return <div
+                key={`dashboard-tab-${ti}`}
+                className={"text-sm text-normal text-black py-2 px-6 cursor-pointer mt-[9px] border-b-[0px] hover:border-b-[1px] hover:text-[#3f4a56]"}
+                style={{
+                    borderBottomStyle: "solid",
+                    borderBottomColor: isActive ? "#408abf" : "#000",
+                    ...extraStyles
+                }}
+                onClick={() => onTabClick(ti)}
+            >
+                {tab}
+            </div>
+        })}
+    </div>
+}
