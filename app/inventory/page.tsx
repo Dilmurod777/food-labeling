b1db0cc000dbb9c096e0d41d03e140d94ae3b7bb
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2024-01-20 20:46:19 +0900

    fit: add inventory customer service section

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -3,14 +3,18 @@ import EmailFreeCourse from "@/app/ui/inventory/email-free-course";
 import Divider from "@/app/ui/divider";
 import FoodManufacturers from "@/app/ui/inventory/food-manufacturers";
 import Features from "@/app/ui/inventory/features";
+import CustomerServiceThatDelivers from "@/app/ui/inventory/customer-service-that-delivers";
 
 
 export default function Inventory() {
-    return <div className={"flex flex-col py-16"}>
+    return <div className={"flex flex-col pt-16 pb-6"}>
         <TopHero/>
         <Divider height={1} heightUnits={"px"} color={"#e7ecef"}/>
         <EmailFreeCourse/>
         <FoodManufacturers/>
         <Features/>
+        <CustomerServiceThatDelivers/>
+        <Divider height={1} heightUnits={"px"} color={"#e7ecef"}/>
+        <EmailFreeCourse/>
     </div>
 }
---
+++ app/ui/inventory/customer-service-that-delivers.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function CustomerServiceThatDelivers() {
+    return <div className={"flex flex-col py-8 px-28 items-center gap-4"}>
+        <h2 className={"text-4xl font-bold text-black"}>Customer service that delivers</h2>
+        <p className={"text-lg font-normal text-black text-center"}>
+            Advice from production and operations experts<br/>
+            Questions answered in minutes, not days<br/>
+            Provide feedback and see it realized
+        </p>
+
+        <div className={"flex flex-col gap-2 items-center mt-6"}>
+            <Link
+                href={"#"}
+                className={"flex items-center justify-center text-[1rem] text-white font-bold py-4 px-8 rounded-md bg-main-green " +
+                    "hover:bg-hover-main-green"}
+            >
+                Start managing inventory like the pros!
+            </Link>
+            <span className={"text-secondary-gray italic"}>or</span>
+            <Link
+                href={"#"}
+                className={"text-lg font-bold text-main-blue hover:text-hover-main-blue"}
+            >
+                Schedule a Chat
+            </Link>
+        </div>
+    </div>
+}
