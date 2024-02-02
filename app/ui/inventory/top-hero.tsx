afc450a271ea153e195e59876d5bc6fbbaa46d78
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2024-02-02 11:23:00 +0900

    local changes

--- app/ui/inventory/top-hero.tsx
+++ app/ui/inventory/top-hero.tsx
@@ -6,8 +6,8 @@ import {GoVideo} from "react-icons/go";
 export default function TopHero() {
     return <div className={"flex flex-col items-center w-full px-12 pb-12"}>
         <h2 className={"text-4xl text-black font-bold mb-3"}>Simple Lot Tracing and Inventory Tracking</h2>
-        <p className={"text-lg text-black font-bold"}>Automated FIFO lot tracing, inventory management, and production planning to run your
-            business</p>
+        <p className={"text-lg text-black font-bold"}>Automated FIFO lot tracing, inventory management, and production
+            planning to run your business</p>
 
         <div className={"flex justify-center gap-16 mt-16"}>
             <Image
@@ -20,14 +20,18 @@ export default function TopHero() {
             />
 
             <div className={"flex flex-col w-1/2 text-xl items-center"}>
-                <p className={"mb-3"}>If you&apos;re not doing lot tracing, you&apos;ve got problems. Food safety is serious business and <strong>you
-                    need the ability to quickly execute a recall.</strong> Foodplanet&apos;s inventory software helps you do just that, scaling
+                <p className={"mb-3"}>If you&apos;re not doing lot tracing, you&apos;ve got problems. Food safety is
+                    serious business and <strong>you
+                        need the ability to quickly execute a recall.</strong> Foodplanet&apos;s inventory software
+                    helps you do just that, scaling
                     with your
                     business (unlike paper or spreadsheets), and saving you time.</p>
-                <p className={"mb-3"}>We help you run and grow your business too — <strong>tracking inventory and how much it&apos;s
+                <p className={"mb-3"}>We help you run and grow your business too — <strong>tracking inventory and how
+                    much it&apos;s
                     worth</strong>,
                     managing production, planning ahead for purchasing, and reporting.</p>
-                <p><strong>With our cloud-based software, all this is a breeze.</strong> Provide role-based access to staff, access it anywhere,
+                <p><strong>With our cloud-based software, all this is a breeze.</strong> Provide role-based access to
+                    staff, access it anywhere,
                     and
                     start managing your business like the pros.</p>
 
