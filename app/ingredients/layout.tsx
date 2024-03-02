ab37130961a09e4baa9a9b664fdccd6c5f80e1e8
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2024-03-02 16:58:29 +0900

    fit: minor ui changes

--- app/ingredients/layout.tsx
+++
@@ -1,8 +0,0 @@
-import Loading from "@/app/ui/loading";
-import React, {ReactNode, Suspense} from "react";
-
-export default function RootLayout({children}: { children: ReactNode }) {
-    return <Suspense fallback={<Loading/>}>
-        {children}
-    </Suspense>
-}
