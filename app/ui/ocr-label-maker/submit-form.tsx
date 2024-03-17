8ce381099570db0b4d91765e82c71be0658edf61
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2024-03-18 08:44:00 +0900

    fit: update ocr

--- app/ui/ocr-label-maker/submit-form.tsx
+++ app/ui/ocr-label-maker/submit-form.tsx
@@ -14,25 +14,49 @@ interface Props {
   user: User;
 }
 
+enum TaskType {
+  Ingredients,
+  Nutrients,
+}
+
 export default function SubmitForm({ user }: Props) {
   const [error, setError] = useState("");
-  const [file, setFile] = useState<File>();
+  const [ingredientsFile, setIngredientsFile] = useState<File>();
+  const [nutrientsFile, setNutrientsFile] = useState<File>();
+  const [ingredients, setIngredients] = useState<string[]>([]);
 
-  const fileUploadHandler = (files: FileList | null) => {
-    console.log(files);
+  const fileUploadHandler = (files: FileList | null, taskType: TaskType) => {
     if (!files || files.length == 0) {
       setError("No file selected. Try again.");
       return;
     }
 
-    setFile(files[0]);
+    switch (taskType) {
+      case TaskType.Ingredients:
+        setIngredientsFile(files[0]);
+        break;
+      case TaskType.Nutrients:
+        setNutrientsFile(files[0]);
+        break;
+    }
   };
 
-  const ocrHandler = async () => {
-    if (!file) return;
+  const ingredientsOCRHandler = async () => {
+    if (!ingredientsFile) return;
 
     const worker = await createWorker("eng");
-    const result = await worker.recognize(URL.createObjectURL(file));
+    const result = await worker.recognize(URL.createObjectURL(ingredientsFile));
+
+    const lines = result.data.lines;
+
+    await worker.terminate();
+  };
+
+  const nutrientsOCRHandler = async () => {
+    if (!nutrientsFile) return;
+
+    const worker = await createWorker("eng");
+    const result = await worker.recognize(URL.createObjectURL(nutrientsFile));
 
     const lines = result.data.lines;
     const searchWords: { [key: string]: number } = {
@@ -94,86 +118,92 @@ export default function SubmitForm({ user }: Props) {
       }
     });
 
-    console.log(searchWords);
-
     await worker.terminate();
   };
 
   return (
-    <div
-      className={
-        "flex flex-grow flex-col items-center justify-center gap-3 px-24 py-12"
-      }
-    >
-      <div className="flex w-full flex-grow items-center justify-center">
-        <label
-          htmlFor="dropzone-file"
-          className="flex h-16 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-white hover:bg-main-gray"
-        >
-          <div className="flex items-center justify-center gap-4 pb-6 pt-5">
-            <svg
-              className="h-8 w-8 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 16"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-              />
-            </svg>
-            <div className={"flex flex-col items-center gap-0"}>
-              <p className="text-sm text-gray-500 dark:text-gray-400">
-                <span className="font-semibold">Click to upload</span> or drag
-                and drop
-              </p>
-              <p className="text-xs text-gray-500 dark:text-gray-400">
-                Only PNG, JPG.
-              </p>
+    <div className={"flex w-full flex-col gap-4 px-12 py-12"}>
+      <div className={"flex w-full items-start gap-12"}>
+        <div className={"flex w-96 flex-col items-center justify-center gap-3"}>
+          <label
+            htmlFor="dropzone-file"
+            className="flex h-16 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-white hover:bg-main-gray"
+          >
+            <div className="flex items-center justify-center gap-4 pb-6 pt-5">
+              <svg
+                className="h-8 w-8 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 16"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                />
+              </svg>
+              <div className={"flex flex-col items-center gap-0"}>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  Only PNG, JPG.
+                </p>
+              </div>
             </div>
-          </div>
-          <input
-            id="dropzone-file"
-            type="file"
-            className="hidden"
-            onChange={(e) => fileUploadHandler(e.target.files)}
-          />
-        </label>
-      </div>
-
-      <div className={"flex items-center justify-center text-center"}>
-        <p className={"text-sm font-bold text-red-500"}>{error}</p>
-      </div>
-
-      {file && (
-        <div className={"flex flex-col items-center gap-6"}>
-          <div
-            className={"bg-red relative h-96 w-96 border-2 border-main-gray"}
-          >
-            <Image
-              src={URL.createObjectURL(file)}
-              alt={"upload-image-preview"}
-              fill
-              style={{
-                objectFit: "contain",
-              }}
+            <input
+              id="dropzone-file"
+              type="file"
+              className="hidden"
+              onChange={(e) =>
+                fileUploadHandler(e.target.files, TaskType.Ingredients)
+              }
             />
+          </label>
+
+          <div className={"flex items-center justify-center text-center"}>
+            <p className={"text-sm font-bold text-red-500"}>{error}</p>
           </div>
 
-          <div
-            onClick={() => ocrHandler()}
-            className={
-              "cursor-pointer rounded-md bg-main-orange px-12 py-3 text-base font-bold text-white hover:bg-hover-main-orange"
-            }
-          >
-            Extract
-          </div>
+          {ingredientsFile && (
+            <div className={"flex w-full flex-col items-center gap-6"}>
+              <div
+                className={
+                  "bg-red relative h-96 w-full border-2 border-main-gray"
+                }
+              >
+                <Image
+                  src={URL.createObjectURL(ingredientsFile)}
+                  alt={"upload-image-preview"}
+                  fill
+                  style={{
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+
+              <div
+                onClick={() => ingredientsOCRHandler()}
+                className={
+                  "cursor-pointer rounded-md bg-main-orange px-12 py-3 text-base font-bold text-white hover:bg-hover-main-orange"
+                }
+              >
+                Extract
+              </div>
+            </div>
+          )}
         </div>
-      )}
+
+        <div className={"flex flex-col gap-2"}>
+          <h2 className={"text-lg font-bold text-black"}>Ingredients</h2>
+
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
