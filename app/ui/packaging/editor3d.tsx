79032e0e8e9086d9fdcc0551df67fa1cc5267f05
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2024-06-10 06:48:37 +0900

    fit: update

--- app/ui/packaging/editor3d.tsx
+++ app/ui/packaging/editor3d.tsx
@@ -14,6 +14,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import ChipsPackageLayout from "@/app/ui/packaging/layouts/chips-package-layout";
 
 interface Props {
   model: Model;
@@ -29,6 +30,11 @@ export default function Editor3D({ model }: Props) {
       case "0":
         return (
           <TelescopeBoxLayout model={model} baseColor={baseColor} size={size} />
+        );
+
+      case "1":
+        return (
+          <ChipsPackageLayout model={model} baseColor={baseColor} size={size} />
         );
       default:
         return (
---
+++ app/ui/packaging/layouts/chips-package-layout.tsx
@@ -0,0 +1,56 @@
+import { Model } from "@/app/lib/3d";
+import { GetHSV } from "@/app/lib/utilities";
+
+interface Props {
+  model: Model;
+  baseColor: number[];
+  size: number[];
+}
+
+export default function ChipsPackageLayout({ model, baseColor, size }: Props) {
+  const base = 150;
+  const width = size[0] * base;
+  const height = size[1] * base;
+  const depth = size[2] * base;
+
+  return (
+    <div className={"flex w-full flex-col items-center gap-8"}>
+      <div className={"flex w-full flex-col gap-6"}>
+        <h1
+          className={
+            "w-full border-b border-b-main-gray pb-2 text-2xl/none font-bold"
+          }
+        >
+          Front/Back:
+        </h1>
+        <div className={"flex w-full items-center justify-center gap-1"}>
+          <div
+            style={{
+              backgroundColor: GetHSV(baseColor),
+              width: `${width}px`,
+              height: `${height}px`,
+            }}
+          />
+        </div>
+      </div>
+      <div className={"flex w-full flex-col gap-6"}>
+        <h1
+          className={
+            "w-full border-b border-b-main-gray pb-2 text-2xl/none font-bold"
+          }
+        >
+          Bottom:
+        </h1>
+        <div className={"flex w-full items-center justify-center gap-1"}>
+          <div
+            style={{
+              backgroundColor: GetHSV(baseColor),
+              width: `${width}px`,
+              height: `${depth}px`,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
